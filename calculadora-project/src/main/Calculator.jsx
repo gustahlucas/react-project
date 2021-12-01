af2679dad3ee4879840d14e1e7fe15cac61790f5
gustahlucas <gustavo.lucas@aluno.ufop.edu.br>
2021-12-01 09:38:27 -0300

    create component display

--- calculadora-project/src/main/Calculator.jsx
+++ calculadora-project/src/main/Calculator.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./Calculator.css";
 import Button from "../components/buttons/Buttons";
+import Display from "../components/display/Displat";
 
 export default class Calculator extends Component {
     render() {
         return (
             <div className="calculator">
+                <Display value={10} />
                 <Button label="AC" />
                 <Button label="/" />
                 <Button label="7" />
