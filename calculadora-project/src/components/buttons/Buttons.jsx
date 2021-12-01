983b9b0ce67ca8f8aad678cab72b71c97df1224f
gustahlucas <gustavo.lucas@aluno.ufop.edu.br>
2021-12-01 09:58:59 -0300

    styled component button

--- calculadora-project/src/components/buttons/Buttons.jsx
+++ calculadora-project/src/components/buttons/Buttons.jsx
@@ -1,5 +1,17 @@
 import "./Buttons.css";
 
 export default function Buttons(props) {
-    return <button className="button"> {props.label}</button>;
+    return (
+        <button
+            className={`button
+        ${props.operation ? "operation" : ""}
+        ${props.double ? "double" : ""}
+        ${props.triple ? "triple" : ""}
+         `}
+            onClick={(e) => props.click(props.label)}
+        >
+            {" "}
+            {props.label}
+        </button>
+    );
 }
